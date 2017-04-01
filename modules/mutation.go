cec3bfd23db74ddaf74e9c3df79deb078841ac2e
Zeu Cxb <zeucxb@gmail.com>
2017-04-01 17:08:20 -0300

    Add some comments and correct the warnigs

--- modules/mutation.go
+++ modules/mutation.go
@@ -6,6 +6,7 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+// MutationType is the root mutation
 var MutationType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Mutation",
 	Fields: graphql.Fields{
--- modules/query.go
+++ modules/query.go
@@ -6,6 +6,7 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+// QueryType is the root query
 var QueryType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Query",
 	Fields: graphql.Fields{
